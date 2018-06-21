080390f16e5f6d3c521af8b3b9d77ca465ce9c69
IndieGameDevRM <rishabhmatharoo11@gmail.com>
2018-06-21 11:15:56 +0530

    level10

---
+++ Assets/ContinueScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ContinueScript : MonoBehaviour {
+    public static string currentlevel = "level1"; 
+
+	public ContinueScript(string c)
+    {
+        currentlevel = c;
+    }
+    public void Continuefromlevel()
+    {
+        SceneManager.LoadSceneAsync(currentlevel);
+    }
+}
--- Assets/Scripts/ScreenChanger.cs
+++ Assets/Scripts/ScreenChanger.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class ScreenChanger : Playbutton {
 	public GameObject poper;
+    private ContinueScript con;
 	void Start(){
+        con = FindObjectOfType<ContinueScript>();
 		//poper.SetActive (true);
 	}
 	public void Next(string Scene){
 		poper.SetActive (false);
 		playButton ();
 		DisableBlocks();
+        con = new ContinueScript(Scene);
         SceneManager.LoadSceneAsync(Scene);
 	}
 
